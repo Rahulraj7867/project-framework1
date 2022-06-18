2ef1fc83c0d706b0e7199538ed7229e6d00d9821
Rahul Rajput <107527961+Rahulraj7867@users.noreply.github.com>
2022-06-18 22:25:26 +0530

    change variable name i to x

--- qspautomationproject/src/programing/LargestNumberbetweenlist.java
+++ qspautomationproject/src/programing/LargestNumberbetweenlist.java
@@ -5,11 +5,11 @@ public class LargestNumberbetweenlist {
 	public static void main(String[] args) {
 		int a[]= {5,10,20,8,4,23};
 		int max= a[0];
-		for (int i=1;i<a.length;i++)
+		for (int x=1;x<a.length;x++)
 		{
-			if(max<=a[i])
+			if(max<=a[x])
 			{
-				max=a[i];
+				max=a[x];
 			}
 			
 		}
