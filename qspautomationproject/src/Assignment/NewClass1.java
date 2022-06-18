b7025ad3ebddcb6a48aa9863ae88820b90ad507a
rahulrajput <rahulrajput@DESKTOP-4PMJEBG>
2022-06-18 22:39:24 +0530

    new variable addi

--- qspautomationproject/src/Assignment/NewClass1.java
+++ qspautomationproject/src/Assignment/NewClass1.java
@@ -3,8 +3,8 @@ package Assignment;
 public class NewClass1 {
 
 	public static void main(String[] args) {
-int x=10;int y=20;
-System.out.println(x+y);
+int datax=10;int datay=20;
+System.out.println(datax+datay);
 	}
 
 }
